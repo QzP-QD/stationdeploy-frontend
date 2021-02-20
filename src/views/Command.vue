a55257ed461266a8dea8e9842331aac9f3173f54
lxm <1192770932@qq.com>
2021-02-20 22:49:06 +0800

    添加拓扑结构

--- src/views/Command.vue
+++ src/views/Command.vue
@@ -1,307 +1,320 @@
 <template>
   <el-container>
-        <el-header>
-            <top-header style="width:100%"></top-header>
-        </el-header>
-        <el-main 
-          style="
+    <el-header>
+      <top-header style="width:100%"></top-header>
+    </el-header>
+    <el-main style="
             width:1100px;
-            display:flex;"
-          >
-          <el-tabs v-model="activeForm"
-            style="margin-right:10px;">
-            <el-tab-pane label="地图信息" name="map">
-              <div 
-                style="
+            display:flex;">
+      <el-tabs v-model="activeForm" style="margin-right:10px;">
+        <el-tab-pane label="地图信息" name="map">
+          <div style="
                   width:780px;
                   height:625px;
                   background-color:red;">
-              </div>
-            </el-tab-pane>
-            <el-tab-pane label="网络拓扑" name="topo">
-              <div 
-                style="
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="网络拓扑" name="topo">
+          <div id="topology" style="
                   width:800px;
                   height:625px;
                   background-color:blue;">
-              </div>
-            </el-tab-pane>
-          </el-tabs>
-          <el-tabs 
-            v-model="activeMission">
-            <el-tab-pane label="图表" name="charts">
-              <div 
-                style="
+          </div>
+        </el-tab-pane>
+      </el-tabs>
+      <el-tabs v-model="activeMission">
+        <el-tab-pane label="图表" name="charts">
+          <div style="
                   width:280px;
                   height:625px;
                   overflow-y:auto;">
-                  <div id="trafficCapacity" 
-                    :style="{width: '280px', height: '300px'}">
-                  </div>
-                  <div id="uploadSpeed" 
-                    :style="{width: '280px', height: '300px'}">
-                  </div>
-                  <div id="downloadSpeed" 
-                    :style="{width: '280px', height: '300px'}">
-                  </div>
-                  <div id="networkDelay" 
-                    :style="{width: '280px', height: '300px'}">
-                  </div>
-              </div>
-            </el-tab-pane>
-            <el-tab-pane label="操作" name="operate">
-              <h4>业务变更:</h4>
-              <el-select v-model="businessValue" placeholder="请选择业务">
-                <el-option
-                  v-for="item in businessList"
-                  :key="item"
-                  :label="item"
-                  :value="item">
-                </el-option>
-              </el-select>
-              <h4>任务变更:</h4>
-              <el-select v-model="missionValue" placeholder="请选择任务">
-                <el-option
-                  v-for="item in missionList"
-                  :key="item"
-                  :label="item"
-                  :value="item">
-                </el-option>
-              </el-select>
-              <el-button
-                style="margin-top:50px;"
-                type="primary" round
-                @click="SubmitChange">
-                确认变更
-              </el-button>
-            </el-tab-pane>
-          </el-tabs>
-        </el-main>
-        <el-main style="width:1100px;">
-          <h4>实际部署对比表</h4>
-          <el-table
-            :data="positionTable"
-            style="width: 100%;">
-            <el-table-column
-              prop="name"
-              label="设备名称"
-              align="center"
-              width="150">
-            </el-table-column>
-            <el-table-column
-              align="center"
-              prop="set_longitude"
-              label="预设经度"
-              width="150">
-            </el-table-column>
-            <el-table-column
-              prop="set_latitude"
-              align="center"
-              label="预设纬度"
-              width="150">
-            </el-table-column>
-            <el-table-column
-              prop="cur_longitude"
-              align="center"
-              label="实际经度"
-              width="150">
-            </el-table-column>
-            <el-table-column
-              prop="cur_latitude"
-              align="center"
-              label="实际纬度"
-              width="150">
-            </el-table-column>
-            <el-table-column
-              prop="dif_longitude"
-              align="center"
-              label="经度差值"
-              width="150">
-            </el-table-column>
-            <el-table-column
-              prop="dif_latitude"
-              align="center"
-              label="纬度差值"
-              width="150">
-            </el-table-column>
-          </el-table>
+            <div id="trafficCapacity" :style="{width: '280px', height: '300px'}">
+            </div>
+            <div id="uploadSpeed" :style="{width: '280px', height: '300px'}">
+            </div>
+            <div id="downloadSpeed" :style="{width: '280px', height: '300px'}">
+            </div>
+            <div id="networkDelay" :style="{width: '280px', height: '300px'}">
+            </div>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="操作" name="operate">
+          <h4>业务变更:</h4>
+          <el-select v-model="businessValue" placeholder="请选择业务">
+            <el-option v-for="item in businessList" :key="item" :label="item" :value="item">
+            </el-option>
+          </el-select>
+          <h4>任务变更:</h4>
+          <el-select v-model="missionValue" placeholder="请选择任务">
+            <el-option v-for="item in missionList" :key="item" :label="item" :value="item">
+            </el-option>
+          </el-select>
+          <el-button style="margin-top:50px;" type="primary" round @click="SubmitChange">
+            确认变更
+          </el-button>
+        </el-tab-pane>
+      </el-tabs>
+    </el-main>
+    <el-main style="width:1100px;">
+      <h4>实际部署对比表</h4>
+      <el-table :data="positionTable" style="width: 100%;">
+        <el-table-column prop="name" label="设备名称" align="center" width="150">
+        </el-table-column>
+        <el-table-column align="center" prop="set_longitude" label="预设经度" width="150">
+        </el-table-column>
+        <el-table-column prop="set_latitude" align="center" label="预设纬度" width="150">
+        </el-table-column>
+        <el-table-column prop="cur_longitude" align="center" label="实际经度" width="150">
+        </el-table-column>
+        <el-table-column prop="cur_latitude" align="center" label="实际纬度" width="150">
+        </el-table-column>
+        <el-table-column prop="dif_longitude" align="center" label="经度差值" width="150">
+        </el-table-column>
+        <el-table-column prop="dif_latitude" align="center" label="纬度差值" width="150">
+        </el-table-column>
+      </el-table>
 
-          <h4>设备运行信息</h4>
-          <el-table
-            :data="deviceWorkingTable"
-            style="width: 100%">
-            <el-table-column
-              prop="name"
-              align="center"
-              label="设备名称"
-              width="150">
-            </el-table-column>
-            <el-table-column
-              prop="type"
-              align="center"
-              label="设备类型"
-              width="150">
-            </el-table-column>
-            <el-table-column
-              prop="status"
-              align="center"
-              label="运行状态"
-              width="150">
-            </el-table-column>
-            <el-table-column
-              prop="uploadspeed"
-              align="center"
-              label="上行速率"
-              width="150">
-            </el-table-column>
-            <el-table-column
-              prop="downloadspeed"
-              align="center"
-              label="下行速率"
-              width="150">
-            </el-table-column>
-            <el-table-column
-              prop="battery"
-              align="center"
-              label="电量"
-              width="150">
-            </el-table-column>
-          </el-table>
-        </el-main>
-    </el-container>
+      <h4>设备运行信息</h4>
+      <el-table :data="deviceWorkingTable" style="width: 100%">
+        <el-table-column prop="name" align="center" label="设备名称" width="150">
+        </el-table-column>
+        <el-table-column prop="type" align="center" label="设备类型" width="150">
+        </el-table-column>
+        <el-table-column prop="status" align="center" label="运行状态" width="150">
+        </el-table-column>
+        <el-table-column prop="uploadspeed" align="center" label="上行速率" width="150">
+        </el-table-column>
+        <el-table-column prop="downloadspeed" align="center" label="下行速率" width="150">
+        </el-table-column>
+        <el-table-column prop="battery" align="center" label="电量" width="150">
+        </el-table-column>
+      </el-table>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
-export default {
-    name:'Command',
-    data(){
-      return{
-        activeForm:"map",        //与name关联
-        activeMission:"charts",
-        businessValue:'',
-        missionValue:'',
-        businessList:'',
-        missionList:'',
-        positionTable:[],
-        deviceWorkingTable:[]
+  import { option } from '../../static/mock/option.js'
+  export default {
+    name: 'Command',
+    data() {
+      return {
+        activeForm: "map", //与name关联
+        activeMission: "charts",
+        businessValue: '',
+        missionValue: '',
+        businessList: '',
+        missionList: '',
+        positionTable: [],
+        deviceWorkingTable: [],
+        topology_data:[],
+        points:[],
+
       }
     },
-    mounted(){
+    mounted() {
       this.getBusinessList();
       this.getMissionList();
       this.getDeviceData();
       this.drawLine();
+      this.getPoints();
+      this.drawTopology();
     },
-    methods:{
-      getBusinessList(){
+    methods: {
+      getBusinessList() {
         var that = this
         this.axios({
           method: 'get',
           url: 'http://localhost:8080/static/mock/businesslist.json'
-          }).then(function (response) {
-            that.businessList = response.data.businessList
-          })
+        }).then(function(response) {
+          that.businessList = response.data.businessList
+        })
       },
-      getMissionList(){
+      getMissionList() {
         var that = this
         this.axios({
           method: 'get',
           url: 'http://localhost:8080/static/mock/missionlist.json'
-          }).then(function (response) {
-            that.missionList = response.data.missionList
-          })
+        }).then(function(response) {
+          that.missionList = response.data.missionList
+        })
       },
-      SubmitChange(){
+      SubmitChange() {
         //提交任务修改信息到后台
       },
-      drawLine(){
+
+      drawLine() {
         var trafficCapacity = this.$echarts.init(document.getElementById('trafficCapacity'))
         trafficCapacity.setOption({
-            title: { text: '设备实时通信量' },
-            tooltip: {},
-            xAxis: {
-                data: ["设备1","设备2","设备3","设备4","设备5","设备6"]
-            },
-            yAxis: {},
-            series: [{
-                name: '通信量',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
+          title: {
+            text: '设备实时通信量'
+          },
+          tooltip: {},
+          xAxis: {
+            data: ["设备1", "设备2", "设备3", "设备4", "设备5", "设备6"]
+          },
+          yAxis: {},
+          series: [{
+            name: '通信量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }]
         });
 
         var uploadSpeed = this.$echarts.init(document.getElementById('uploadSpeed'))
         uploadSpeed.setOption({
-            title: { text: '设备上行速率' },
-            tooltip: {},
-            xAxis: {
-                data: ["设备1","设备2","设备3","设备4","设备5","设备6"]
-            },
-            yAxis: {},
-            series: [{
-                name: '速率',
-                type: 'line',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
+          title: {
+            text: '设备上行速率'
+          },
+          tooltip: {},
+          xAxis: {
+            data: ["设备1", "设备2", "设备3", "设备4", "设备5", "设备6"]
+          },
+          yAxis: {},
+          series: [{
+            name: '速率',
+            type: 'line',
+            data: [5, 20, 36, 10, 10, 20]
+          }]
         });
 
         var downloadSpeed = this.$echarts.init(document.getElementById('downloadSpeed'))
         downloadSpeed.setOption({
-            title: { text: '设备下行速率' },
-            tooltip: {},
-            xAxis: {
-                data: ["设备1","设备2","设备3","设备4","设备5","设备6"]
-            },
-            yAxis: {},
-            series: [{
-                name: '速率',
-                type: 'line',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
+          title: {
+            text: '设备下行速率'
+          },
+          tooltip: {},
+          xAxis: {
+            data: ["设备1", "设备2", "设备3", "设备4", "设备5", "设备6"]
+          },
+          yAxis: {},
+          series: [{
+            name: '速率',
+            type: 'line',
+            data: [5, 20, 36, 10, 10, 20]
+          }]
         });
 
         var networkDelay = this.$echarts.init(document.getElementById('networkDelay'))
         networkDelay.setOption({
-            title: { text: '设备网络时延' },
-            tooltip: {},
-            xAxis: {
-                data: ["设备1","设备2","设备3","设备4","设备5","设备6"]
-            },
-            yAxis: {},
-            series: [{
-                name: '时延',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
+          title: {
+            text: '设备网络时延'
+          },
+          tooltip: {},
+          xAxis: {
+            data: ["设备1", "设备2", "设备3", "设备4", "设备5", "设备6"]
+          },
+          yAxis: {},
+          series: [{
+            name: '时延',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }]
         });
       },
-      getDeviceData(){
+      getDeviceData() {
         var that = this
         this.axios({
           method: 'get',
           url: 'http://localhost:8080/static/mock/devicepositiondata.json'
-          }).then(function (response) {
-            that.positionTable = response.data.positionTable
-          })
+        }).then(function(response) {
+          that.positionTable = response.data.positionTable
+        })
 
         this.axios({
           method: 'get',
           url: 'http://localhost:8080/static/mock/deviceworkingdata.json'
-          }).then(function (response) {
-            that.deviceWorkingTable = response.data.deviceWorkingTable
-          })
+        }).then(function(response) {
+          that.deviceWorkingTable = response.data.deviceWorkingTable
+        })
+      },
+      getPoints(){
+        var that=this
+        var points=[]
+        this.axios({
+          method: 'get',
+          url: 'http://localhost:8080/static/mock/suggestData.json'//需要替换成修改后的值
+        }).then(function(response) {
+          that.topology_data = response.data.suggest_location
+          for(let i = 0; i < that.topology_data.length; i++){
+            const node1={
+              name:that.topology_data[i].device_mapID,
+              draggable:true,
+              category:1,
+            }
+
+            points.push(node1)
+            //option.series[0].data.push(node)
+            // var node={name:'设备'+i,category: 0,draggable: true,}
+            // option.series[0].data.push(node)
+
+          }
+
+          that.points=points
+        })
+
+      },
+      drawTopology() {//生成设备拓扑图
+      var that=this
+        var topology = this.$echarts.init(document.getElementById('topology'))
+        topology.setOption(option);
+        that.axios
+        .get('http://localhost:8080/static/mock/topology.json')
+        .then(res=>{
+          setTimeout(()=>{
+            console.log(res);
+            //console.log(res.data.links);
+            topology.setOption({
+              series:[{
+                data:res.data.data,
+                links:res.data.links
+              }]
+            })
+
+          },500);
+        }).catch(err=>{
+          console.log(err);
+        });
+
+        // option.series[0].data=points;
+
+        //console.log(points)
+
+
+        //node.name='设备6'
+        //option.series[0].data.push(node)
+
+        // console.log("option：")
+        // console.log(option.series[0])
+        // console.log("points：")
+
+        // for(let i=0;i<7;i++){
+        //   var node={name:'设备'+i,category: 0,draggable: true,}
+        //   option.series[0].data.push(node)
+
+        // }
+        //console.log(that.points);
+
+        console.log(option.series[0])
+        //topology.setOption(option)
+
+
       }
-    }
-}
+
+
+    },
+
+  }
 </script>
 
 <style>
-  .el-header, .el-footer {
+  .el-header,
+  .el-footer {
     background-color: #B3C0D1;
     color: #999999;
-    
+
   }
-  
+
   .el-main {
     background-color: #E9EEF3;
     color: #333;
---
+++ static/mock/option.js
@@ -0,0 +1,127 @@
+export const option = {
+          series: [{
+            type: 'graph',
+            layout: 'force',
+            legendHoverLink: true, //是否启用图例 hover(悬停) 时的联动高亮。
+            hoverAnimation: true, //是否开启鼠标悬停节点的显示动画
+            coordinateSystem: null, //坐标系可选
+            xAxisIndex: 0, //x轴坐标 有多种坐标系轴坐标选项
+            yAxisIndex: 0, //y轴坐标
+            force: {
+              repulsion: 450, //相距距离
+              edgeLength: [150, 200],
+              layoutAnimation: false
+            },
+            roam: true, //是否开启鼠标缩放和平移漫游。默认不开启。如果只想要开启缩放或者平移，可以设置成 'scale' 或者 'move'。设置成 true 为都开启
+            nodeScaleRatio: 0.6, //鼠标漫游缩放时节点的相应缩放比例，当设为0时节点不随着鼠标的缩放而缩放
+            draggable: true, //节点是否可拖拽，只在使用力引导布局的时候有用。
+            focusNodeAdjacency: true, //是否在鼠标移到节点上的时候突出显示节点以及节点的边和邻接节点。
+            symbolSize: 50, //图形大小
+            edgeSymbolSize: 10, //边两端的标记大小，可以是一个数组分别指定两端，也可以是单个统一指定。
+            itemStyle: { //===============图形样式，有 normal 和 emphasis 两个状态。normal 是图形在默认状态下的样式；emphasis 是图形在高亮状态下的样式，比如在鼠标悬浮或者图例联动高亮时。
+              normal: { //默认样式
+                label: {
+                  show: true
+                },
+                //borderType : 'solid', //图形描边类型，默认为实线，支持 'solid'（实线）, 'dashed'(虚线), 'dotted'（点线）。
+                //borderColor : 'rgba(255,215,0,0.4)', //设置图形边框为淡金色,透明度为0.4
+                //borderWidth : 2, //图形的描边线宽。为 0 时无描边。
+                // opacity : 1 // 图形透明度。支持从 0 到 1 的数字，为 0 时不绘制该图形。默认0.5
+
+              },
+              emphasis: { //高亮状态
+
+              }
+            },
+            lineStyle: { //==========关系边的公用线条样式。
+              normal: {
+                color: '#31354B',
+                width: '1',
+                type: 'solid', //线的类型 'solid'（实线）'dashed'（虚线）'dotted'（点线）
+                curveness: 0, //线条的曲线程度，从0到1
+                opacity: 1
+                // 图形透明度。支持从 0 到 1 的数字，为 0 时不绘制该图形。默认0.5
+              },
+              emphasis: { //高亮状态
+
+              }
+            },
+            label: { //=============图形上的文本标签
+              normal: {
+                show: true, //是否显示标签。
+                position: 'bottom', //标签的位置。['50%', '50%'] [x,y]   'inside'
+                textStyle: { //标签的字体样式
+                  color: '#2D2F3B', //字体颜色
+                  fontStyle: 'normal', //文字字体的风格 'normal'标准 'italic'斜体 'oblique' 倾斜
+                  fontWeight: 'bolder', //'normal'标准'bold'粗的'bolder'更粗的'lighter'更细的或100 | 200 | 300 | 400...
+                  fontFamily: 'sans-serif', //文字的字体系列
+                  fontSize: 12, //字体大小
+                }
+              },
+              emphasis: { //高亮状态
+
+              }
+            },
+            edgeLabel: { //==============线条的边缘标签
+              normal: {
+                show: false
+              },
+              emphasis: { //高亮状态
+
+              }
+            },
+            categories: [{
+              name: '',
+              itemStyle: {
+                normal: {
+                  color: "#009800"
+                }
+              }
+            }, {
+              name: '',
+              itemStyle: {
+                normal: {
+                  color: "#55aa7f"
+                }
+              }
+            }],
+            label: {
+              normal: {
+                show: true,
+                textStyle: {
+                  fontSize: 12
+                },
+              }
+            },
+
+            // data: [{
+            //   name: '设备1',
+            //   category: 0,
+            //   draggable: true,
+            // },{
+            //   name: '设备2',
+            //   category: 0,
+            //   draggable: true,
+            // },{
+            //   name: '设备3',
+            //   category: 0,
+            //   draggable: true,
+            // },{
+            //   name: '设备4',
+            //   category: 0,
+            //   draggable: true,
+            // },{
+            //   name: '设备5',
+            //   category: 0,
+            //   draggable: true,
+            // },{
+            //   name: '设备6',
+            //   category: 0,
+            //   draggable: true,
+            // },],
+            data:[],
+            links: [],
+
+          }]
+
+        }
